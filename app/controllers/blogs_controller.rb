193ea2ab9b919f03553e2e4e9849a840b22cf1a9
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-08 14:05:56 +0900

    詳細ページ追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,6 +17,9 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+  def show
+    @blog = Blog.find(params[:id])
+  end
   private
   def blog_params
     params.require(:blog).permit(:content, :image)
