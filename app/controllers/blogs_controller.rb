20a7d2907d15990fb3d775a4672e47423b1bc48c
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-08 12:26:33 +0900

    createアクション追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,20 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+  def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+      render.new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "投稿しました！"
+      else
+        render.new
+      end
+    end
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:comtent, :image)
+  end
 end
