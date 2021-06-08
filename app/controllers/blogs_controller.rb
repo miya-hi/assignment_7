3ef968ae6fe5b79ad9225cebef71e3c2f8850f4c
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-08 17:50:29 +0900

    画像表示のタグ追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -39,7 +39,7 @@ class BlogsController < ApplicationController
   end
   private
   def blog_params
-    params.require(:blog).permit(:content, :image)
+    params.require(:blog).permit(:content, :image, :image_cache)
   end
   def set_blog
     @blog = Blog.find(params[:id])
